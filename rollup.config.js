15c49a5365db744045c91106411a805812537d63
Tim Dirks <tim.dirks@hotmail.com>
2022-06-20 10:08:40 +0200

    Remove stuff

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import babel from "@rollup/plugin-babel";
 import copy from "rollup-plugin-copy";
 import del from "rollup-plugin-delete";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
 export default [
@@ -11,53 +10,21 @@ export default [
     input: "src/quill.mention.js",
     output: [
       {
-        file: "docs/quill.mention.min.js",
-        format: "iife",
-        name: "quillMention",
-        plugins: [terser()],
-        globals: {
-          quill: "Quill",
-        },
+        file: `dist/${pkg.entry}.js`,
+        format: "umd",
+        name: "QuillMention",
       },
       {
-        file: "dist/quill.mention.min.js",
-        format: "iife",
-        name: "quillMention",
-        plugins: [terser()],
-        globals: {
-          quill: "Quill",
-        },
+        file: `dist/${pkg.entry}.mjs`,
+        format: "esm",
+        name: "QuillMention",
       },
     ],
     external: ["quill"],
     plugins: [
       del({
-        targets: ["docs/*", "dist/*"],
+        targets: "dist/*",
       }),
-      localResolve(),
-      babel({
-        exclude: ["node_modules/**"],
-      }),
-      postcss({
-        extract: true,
-        minimize: true,
-      }),
-    ],
-  },
-  {
-    input: "src/quill.mention.js",
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-      },
-      {
-        file: pkg.module,
-        format: "es",
-      },
-    ],
-    external: ["quill"],
-    plugins: [
       localResolve(),
       babel({
         exclude: ["node_modules/**"],
